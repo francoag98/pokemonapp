43186b8f40557f8b87669d4f21355564c88f09cd
Franco Aglieri <francoaglieri@hotmail.com>
2022-10-09 20:57:22 -0300

    back 60%

--- api/src/controllers/getController.js
+++ api/src/controllers/getController.js
@@ -1,35 +1,67 @@
-const fetch = require("node-fetch")
+const fetch = require("node-fetch");
+const { Pokemon } = require("../db");
 
-const getAll= async ()=>{
-    const pokemones = await fetch("https://pokeapi.co/api/v2/pokemon?limit=40")
-    .then( async response=> await response.json())
+const getAll = async () => {
+  const pokemones = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=40"
+  ).then((response) => response.json());
 
-    const results = pokemones.results
-    let pokemonesApi = [];
-    results.forEach(async pokemon =>{
-        if(!pokemon) throw Error("pokemons dont exist");
-        if(pokemon.url){
-            const pokemoon = await fetch(pokemon.url)
-            .then( response=>  response.json())
-            pokemonesApi.push({
-                id: pokemoon.id,
-                name: pokemoon.name,
-                hp: pokemoon.stats[0].base_stat,
-                attack: pokemoon.stats[1].base_stat,
-                defense: pokemoon.stats[2].base_stat,
-                speed: pokemoon.stats[5].base_stat,
-                height: pokemoon.height,
-                weight: pokemoon.weight,
-                img: pokemoon.sprites.front_default,
-                type: pokemoon.types.map(e => e.type.name)
-            })
-        }
-    })
-    return pokemonesApi
-    console.log(pokemonesApi);
-}
+  const results = pokemones.results;
+  let pokemonesApi = [];
 
+  for (let i = 0; i < results.length; i++) {
+    if (!results[i]) return pokemonesApi;
+    if (results[i].url) {
+      const pokemoon = await fetch(results[i].url).then((response) => {
+        return response.json();
+      });
+      pokemonesApi.push({
+        id: pokemoon.id,
+        name: pokemoon.name,
+        hp: pokemoon.stats[0].base_stat,
+        attack: pokemoon.stats[1].base_stat,
+        defense: pokemoon.stats[2].base_stat,
+        speed: pokemoon.stats[5].base_stat,
+        height: pokemoon.height,
+        weight: pokemoon.weight,
+        img: pokemoon.sprites.front_default,
+        type: pokemoon.types.map((e) => e.type.name),
+      });
+    }
+  }
+  return pokemonesApi;
+};
+
+const getOne = async (id) => {
+  if (!id) throw Error("Info missing");
+  try {
+    const pokeDb = await Pokemon.findByPk(id);
+    if (id === pokeDb.id) return pokeDb;
+  } catch (error) {
+    const pokeapi = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(
+      (response) => response.json()
+    );
+    if (pokeapi.id === parseInt(id)) return pokeapi;
+  }
+};
+
+const getByName = async (name) => {
+  if (!name) throw Error("Info missing");
+  try {
+    const pokeDbName = Pokemon.findOne({ where: { name: name } });
+    console.log(pokeDbName);
+    if (name === pokeDbName.name) return pokeDbName;
+  } catch (error) {
+    const pokeapiname = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?name=${name}`
+    ).then((response) => response.json());
+    if (name === pokeapiname.name) return pokeapiname;
+    console.log(pokeapiname);
+  }
+};
 
 module.exports = {
-    getAll
-}
+  getAll,
+  getOne,
+  getByName,
+};
--- api/src/routes/getRouter.js
+++ api/src/routes/getRouter.js
@@ -1,16 +1,33 @@
 const routers = require("express").Router();
-const {getAll} = require("../controllers/getController")
+const { getAll, getOne, getByName } = require("../controllers/getController");
 
-routers.get("/",async (req,res)=>{
-    try {
-        const pokemons = await getAll()
-        if(!pokemons) throw Error("Pokemon does not exist")
-        console.log(pokemons);
-        res.status(200).json(pokemons)
-    } catch (error) {
-        res.status(400).send({message: error.message})
-    }
-})
+routers.get("/", async (req, res) => {
+  try {
+    const pokemons = await getAll();
+    if (!pokemons) throw Error("Pokemon does not exist");
+    res.status(200).json(pokemons);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+routers.get("/", async (req, res) => {
+  const { name } = req.query;
+  try {
+    const pokemon = await getByName(name);
+    console.log(pokemon);
+    res.status(200).send(pokemon);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+routers.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const pokemon = await getOne(id);
+    res.status(200).send(pokemon);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
 
-
-module.exports = routers
+module.exports = routers;
